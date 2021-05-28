bb03ac347a7bdf5cfda0e1513c4c3d4f04a232c2
Maria Hristova <36624401+MariaHr8@users.noreply.github.com>
2021-05-28 03:27:47 +0100

    Corresponding titles

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function imgSlider(anything) {
     document.querySelector('.sign').src = anything;
+
+    //getting image name
+    var string = getSignName();
+
+    document.querySelector('.title').innerHTML = "<span>" + string + "</span>";
+}
+
+function getSignName() {
+    var str = event.target.src;
+    var res = str.split("/");
+    var final = res[4].split(".");
+    var stringName = final[0].charAt(0).toUpperCase() + final[0].slice(1);
+    return stringName;
 }
